4afa90176e72e31bddd95a5de89d8df3cbd22651
Fülöp Márton <fulopmarton99@gmail.com>
2022-01-06 21:06:47 +0200

    style change

--- frontend/src/components/ElevatorStatusDisplay/ElevatorStatusDisplay.styles.js
+++ frontend/src/components/ElevatorStatusDisplay/ElevatorStatusDisplay.styles.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+  width: 20px;
+  text-align: center;
+`;
 
 export const LiftName = styled.div`
   background: green;
